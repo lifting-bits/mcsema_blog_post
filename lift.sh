8c5ff6a49b4d1d0b61cada5a352706f4e64771d4
Artem Dinaburg <artem@trailofbits.com>
2018-09-09 04:28:01 -0400

    More thorough checking of prerequisites in lift.sh

--- lift.sh
+++ lift.sh
@@ -5,19 +5,19 @@
 # pip install pyelftools
 
 # Example entries:
-# Mcsema installed to a virtualenv in /store/artem/ve
-#MCSEMA_DIR=/store/artem/ve
-# Remill source was cloned to /store/artem/ve/git/remill
-#MCSEMA_SRC=/store/artem/ve/git/remill
-# We are using the LLVM 4.0 toolchain
-#LLVM_VERSION=4.0
+# Mcsema installed to a virtualenv in /store/artem/diversity
+#MCSEMA_DIR=/store/artem/diversity
+# McSema source in the remill directory at /store/artem/diversity/remill/tools/mcsema
+#MCSEMA_SRC=/store/artem/diversity/remill/tools/mcsema
+# For now, We must use the LLVM 3.8 toolchain as thats what the multicompiler ships
+LLVM_VERSION=3.8
 # Location of the multicompiler
-#MCOMP_DIR=/data/opt/mcomp/bin
+#MCOMP_DIR=/store/artem/diversity/multicompiler/install/bin/
 
-#MCSEMA_DIR=
-#MCSEMA_SRC=
-#LLVM_VERSION=
-#MCOMP_DIR=
+# Set values for your installation here
+MCSEMA_DIR=
+MCSEMA_SRC=
+MCOMP_DIR=
 
 CXX=clang++-${LLVM_VERSION}
 LIFTER=${MCSEMA_DIR}/bin/mcsema-lift-${LLVM_VERSION}
@@ -26,7 +26,9 @@ ABI_DIR=${MCSEMA_DIR}/share/mcsema/${LLVM_VERSION}/ABI/linux/
 IN_DIR=$(pwd)
 IN_FILE=example
 OUT_DIR=$(pwd)
-IDA_DIR=/opt/ida-6.7
+# This should be your IDA installation directory.
+#IDA_DIR=/home/artem/ida-6.9
+IDA_DIR=
 
 function sanity_check
 {
@@ -34,6 +36,18 @@ function sanity_check
   if [[ -z "${MCSEMA_DIR}" ]]
   then
     echo "Please edit this script and set MCSEMA_DIR to the mcsema *installation* directory"
+    exit 1
+  fi
+
+  local abi_lib="${ABI_DIR}/ABI_exceptions_amd64.bc"
+  if [[ ! -f "${abi_lib}" ]]
+  then
+    echo "ABI library for exceptions not found (checked: [${abi_lib}])."
+    echo "Please rebuild mcsema via: "
+    echo ""
+    echo "    cd ${MCSEMA_SRC}/../../remill-build"
+    echo "    cmake -DMCSEMA_DISABLED_ABI_LIBRARIES:STRING=\"\" .."
+    echo "    make -j`nproc` install"
     exit 1
   fi
 
@@ -47,6 +61,32 @@ function sanity_check
   then
     echo "Please edit this script and set LLVM_VERSION to the desired LLVM version (e.g. 4.0)"
     exit 1
+  fi
+
+  if [[ ! -f "${LIFTER}" ]]
+  then
+    echo "Could not find McSema installation. Looked for [${LIFTER}]"
+    exit 1
+  fi
+
+  if [[ "${1}" == "diversify" ]]
+  then
+    local mcomp_bin="${MCOMP_DIR}/clang++"
+    if [[ ! -f "${mcomp_bin}" ]]
+    then
+      echo "Could not find multicompiler. Looked for it in [${mcomp_bin}]"
+      echo "Please set MCOMP_DIR in this script to the multicompiler's installation directory"
+      exit 1
+    fi
+
+    ${MCOMP_DIR}/clang++ --version | grep -q "clang version ${LLVM_VERSION}"
+    if [ $? -ne 0 ]
+    then
+      echo "Version mismatch between Multicompiler and McSema"
+      echo "    Multicompiler: `${MCOMP_DIR}/clang++ --version | grep -o 'clang version ...'`"
+      echo "    McSema: ${LLVM_VERSION}"
+      exit 1
+    fi
   fi
 
 }
@@ -122,8 +162,7 @@ function diversify_binary
   local in_file=${1}
   local RANDOM_SEED=42
   local MCOMP_CFLAGS="-flto -fuse-ld=gold -frandom-seed=${RANDOM_SEED} -g -O0 -fno-slp-vectorize"
-  local MCOMP_LDFLAGS="-Wl,--random-seed=${RANDOM_SEED} \
-    -g \
+  local MCOMP_LDFLAGS="-g \
     -Wl,--plugin-opt,-random-seed=${RANDOM_SEED} \
     -Wl,--plugin-opt,disable-vectorization"
 
@@ -144,7 +183,7 @@ function diversify_binary
 }
 
 
-sanity_check
+sanity_check "${1}"
 
 clean_and_build ${IN_FILE}
 
@@ -157,6 +196,6 @@ lift_binary ${IN_FILE}
 new_binary ${IN_FILE}
 
 if [[ "${1}" == "diversify" ]]
-then 
+then
   diversify_binary ${IN_FILE}
 fi
